68894030a1d0076cf073ecd4b4d012999f343419
mgmayagu <mmayagu000@citymail.cuny.edu>
2021-02-18 20:58:10 -0500

    storyboard

---
+++ flixster/MoviewCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MoviewCellTableViewCell.swift
+//  flixster
+//
+//  Created by Michael Mayaguari on 2/18/21.
+//
+
+import UIKit
+
+class MoviewCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
